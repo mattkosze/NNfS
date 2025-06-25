992e7e606e28d7da7b4f8e0606db4046e9945f50
mattkosze <matthew.koszewicz@gmail.com>
2025-06-24 22:21:32 -0400

    Chapter sixteen finished; sigmoid & bcel classes added

--- resources/classes.py
+++ resources/classes.py
@@ -304,3 +304,41 @@ class DropoutLayer:
         # The gradient
         self.dinputs = dvalues * self.binaryMask
 
+
+class Sigmoid:
+    def forward(self, inputs):
+        # Save input and calculate output
+        self.inputs = inputs
+        self.output = 1 / (1 + np.exp(-inputs))
+
+    def backward(self, dvalues):
+        # Calculate the derivative
+        self.dinputs = dvalues * (1 - self.output) * self.output
+
+
+class BinaryCrossEntropyLoss(Loss):
+    # Forward pass
+    def forward(self, y_pred, y_true):
+        # Clip data on both sides to prevent division by 0, both sides to prevent skewing data
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Calculate sample-wise losses
+        sample_losses = -(y_true * np.log(y_pred_clipped) + (1 - y_true) * np.log(1 - y_pred_clipped))
+        sample_losses = np.mean(sample_losses, axis=-1)
+
+        return sample_losses
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+        # Sample size
+        samples = len(dvalues)
+        # Number of outputs per sample
+        outputs = len(dvalues[0])
+
+        # Clip data
+        clipped_dvalues = np.clip(dvalues, 1e-7, 1 - 1e-7)
+
+        # Calculate gradient
+        self.dinputs = -(y_true / clipped_dvalues - (1 - y_true) / (1 - clipped_dvalues)) / outputs
+        # Normalize gradient
+        self.dinputs /= samples
